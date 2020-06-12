6087a7d838082fa46424237fb7a8f935727887ef
Gonzalo Carrillo <ggcarrillo1023@gmail.com>
2020-06-12 16:05:02 -0400

    Add Article scaffold

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
+import improveDropdown from '../components/select';
+
+improveDropdown();
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  root to: 'articles#index'
+  resources :articles, except: :index
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :reviews, only: [:create]
+  end
+  resources :doses, only: [:destroy]
 end

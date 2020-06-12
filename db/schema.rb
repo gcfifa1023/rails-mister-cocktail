2526e912f35cec6439bf3dfa7678013641606fd8
Gonzalo Carrillo <ggcarrillo1023@gmail.com>
2020-06-12 17:29:49 -0400

    deleted articles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_195805) do
+ActiveRecord::Schema.define(version: 2020_06_11_170843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"

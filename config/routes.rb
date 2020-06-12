58c91a2b7ac29b8f2c0f30f9a268558b9ce832cd
Gonzalo Carrillo <ggcarrillo1023@gmail.com>
2020-06-12 17:24:29 -0400

    deleted articles model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  root to: 'articles#index'
-  resources :articles, except: :index
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:create]
-    resources :reviews, only: [:create]
   end
   resources :doses, only: [:destroy]
 end
